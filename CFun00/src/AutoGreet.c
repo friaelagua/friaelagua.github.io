fb53653a4917a87ff8d375e0eb04a7ca93449255
friaelagua <friaelagua@outlook.com>
2025-07-26 15:31:51 +0800

    OK

--- CFun00/src/AutoGreet.c
+++ CFun00/src/AutoGreet.c
@@ -4,8 +4,7 @@
 int main() {
     time_t rawtime;
     struct tm *info;
-	int hour;
-	char buf[256];
+	int hour,minute;
 
     // Get the current time
     time(&rawtime);
@@ -14,6 +13,7 @@ int main() {
     info = localtime(&rawtime);
 
 	hour=info->tm_hour;
+	minute=info->tm_min;
 
 	if (hour >= 5 && hour < 12) {
         printf("Good Morning!");
@@ -27,9 +27,30 @@ int main() {
     }
     printf("\n");
 
-	strftime(buf, sizeof(buf), "%l:%M %p", info);
+	// Adjust hour for 12-hour format if needed
+    if (hour == 0) {
+        hour = 12; // 00:MM is 12 AM
+    } else if (hour > 12) {
+        hour -= 12; // Convert to 12-hour format
+    }
 
-	printf("The time is %s.\n",buf);
+    if (minute == 0) {
+        printf("It is %d o'clock.\n", hour);
+    } else if (minute == 15) {
+        printf("It is quarter past %d.\n", hour);
+    } else if (minute == 30) {
+        printf("It is half past %d.\n", hour);
+    } else if (minute == 45) {
+     // For 'quarter to', the hour should be the next hour
+		printf("It is quarter to %d.\n", (hour % 12) + 1);
+    } else if (minute < 30) {
+        printf("It is %d minutes past %d.\n", minute, hour);
+    } else { // minute > 30
+        printf("It is %d minutes to %d.\n", 60 - minute, (hour % 12) + 1);
+    }
+
+
+
 
     return 0;
 }
